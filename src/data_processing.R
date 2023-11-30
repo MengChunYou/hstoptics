79089f133e30a5cf19e306d7f3ac5e34e83e1785
MengChunYou <angeladk394@gmail.com>
2023-12-01 00:10:04 +0800

    [Add] holidays_2022.csv

--- src/data_processing.R
+++ src/data_processing.R
@@ -1,5 +1,13 @@
 # data_processing.R
 
-#  filter study area polygons 
+library(sf)
+library(dplyr)
 
-#  filter study area accidents
+# read raw SHP data and filter study area polygons 
+village_polygons <- 
+  paste("data/empirical/raw/taiwan_village_polygons/",
+        "VILLAGE_NLSC_121_1120317.shp", sep = "") %>% 
+  st_read(.) %>% 
+  filter(COUNTYNAME %in% c("臺北市", "新北市", "基隆市"))
+
+# filter study area accidents
