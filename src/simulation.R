da4bb0c9ce2f33a6072703c0021ac5d7fd6480a7
MengChunYou <angeladk394@gmail.com>
2023-12-07 12:25:43 +0800

    [Modify] src/simulation.R

---
+++ src/simulation.R
@@ -0,0 +1,53 @@
+# simulation.R
+
+
+## One Cluster
+
+### One Ellipsoid
+
+### One Linear Structure
+
+### One Cylinder
+
+### One Horizontal Cross Structure
+
+### One Vertical Cross Structure
+
+
+## Two Clusters
+
+### Two Ellipsoids
+
+### Two Linear Structures
+
+### Two Cylinders
+
+### Two Horizontal Cross Structures
+
+### Two Vertical Cross Structures
+
+
+## Two Overlapping Faulted Clusters
+
+### Two Overlapping Faulted Ellipsoids
+
+### Two Overlapping Faulted Linear Structures
+
+### Two Overlapping Faulted Cylinders
+
+### Two Overlapping Faulted Horizontal Cross Structures
+
+### Two Overlapping Faulted Vertical Cross Structures
+
+
+## Two Overlapping Smooth Clusters
+
+### Two Overlapping Smooth Ellipsoids
+
+### Two Overlapping Smooth Linear Structures
+
+### Two Overlapping Smooth Cylinders
+
+### Two Overlapping Smooth Horizontal Cross Structures
+
+### Two Overlapping Smooth Vertical Cross Structures
