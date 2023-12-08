691c74040d15467fdd6826bd217acdcd098e899f
MengChunYou <angeladk394@gmail.com>
2023-12-08 15:04:47 +0800

    [Add] simulated data (feature combination 1, 3, 4)

--- src/simulation.R
+++ src/simulation.R
@@ -16,6 +16,8 @@ wirte_simulated_data = function(simulated_data, file_order){
     row.names = F)
 }
 
+n_points <- 200
+
 ## One Cluster
 
 ### One Ellipsoid
@@ -23,7 +25,7 @@ wirte_simulated_data = function(simulated_data, file_order){
 set.seed(123)
 
 simulated_data <- 
-  mvrnorm(100, 
+  mvrnorm(n_points, 
           mu = c(0, 0, 0), 
           Sigma = matrix(c(1, 0, 0, 0, 2, 0, 0, 0, 3), ncol = 3)) %>% 
   as.data.frame() %>% 
@@ -37,16 +39,48 @@ wirte_simulated_data(simulated_data, 2)
 
 ### One Cylinder
 
+# 生成虛擬資料
+set.seed(123)
+x <- rnorm(n_points)
+y <- rnorm(n_points)
+t <- seq(0, 2 * pi, length.out = n_points)
+
+# 設定圓筒型的半徑和高度
+radius <- 1.5
+height <- 2
+
+# 將資料轉換成圓筒型
+simulated_data <- data_frame(
+  x = radius * cos(t),
+  y = radius * sin(t),
+  t = sample(seq(0, height, length.out = n_points), n_points, T)
+)
+
 wirte_simulated_data(simulated_data, 3)
 
 ### One Horizontal Cross Structure
+
+# 生成虛擬資料
+set.seed(123)
+x <- rnorm(n_points)
+y <- rnorm(n_points)
+t <- rnorm(n_points)
+
+# 設定十字體的尺寸
+cross_size <- 2
+
+# 將資料轉換成橫躺的十字體
+simulated_data <- data.frame(
+  x = c(rep(0, n_points), cross_size * x),
+  y = c(cross_size * y, rep(0, n_points)),
+  t = c(rep(t, each = 2))
+)
 
 wirte_simulated_data(simulated_data, 4)
 
 ### One Vertical Cross Structure
 
 wirte_simulated_data(simulated_data, 5)
-
 
 ## Two Clusters
 
