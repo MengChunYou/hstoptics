53fd8b17bcb4458d06e4640f0f4af60b09a7a0bc
MengChunYou <angeladk394@gmail.com>
2023-12-14 21:52:56 +0800

    [Update] simulation.R ## Two Clusters

--- src/simulation.R
+++ src/simulation.R
@@ -26,6 +26,7 @@ wirte_simulated_data = function(simulated_data, file_order){
       sep = ""
     ),
     row.names = F)
+  message(paste("write", nrow(simulated_data), "data points"))
 }
 
 n_points <- 200
@@ -52,9 +53,9 @@ set.seed(123)
 line_length <- 3
 
 simulated_data <- data.frame(
-  x = rnorm(n_points / 2, mean = 0, sd = 0.5),
-  y = line_length * rnorm(n_points / 2),
-  t = rnorm(n_points / 2)
+  x = rnorm(n_points, sd = 0.5),
+  y = line_length * rnorm(n_points),
+  t = rnorm(n_points, sd = 0.5)
 )
 
 wirte_simulated_data(simulated_data, 2)
@@ -65,12 +66,12 @@ set.seed(123)
 
 z <- seq(0, 2 * pi, length.out = n_points)
 radius <- 4
-noise <- rnorm(n_points, mean = 0, sd = 0.5)
+noise <- rnorm(n_points, sd = 0.5)
 
 simulated_data <- data.frame(
   x = (radius + noise) * cos(z) ,
   y = (radius + noise) * sin(z) ,
-  t = rnorm(n_points, mean = 0, sd = 3)
+  t = rnorm(n_points) * 3
 )
 
 wirte_simulated_data(simulated_data, 3)
@@ -82,10 +83,10 @@ set.seed(123)
 cross_size <- 3
 
 simulated_data <- data.frame(
-  x = c(rnorm(n_points / 2, mean = 0, sd = 0.5), 
+  x = c(rnorm(n_points / 2, sd = 0.5), 
         cross_size * rnorm(n_points / 2)),
   y = c(cross_size * rnorm(n_points / 2), 
-        rnorm(n_points / 2, mean = 0, sd = 0.5)),
+        rnorm(n_points / 2, sd = 0.5)),
   t = rnorm(n_points) * 3
 )
 
@@ -98,12 +99,11 @@ set.seed(123)
 line_length <- 3
 
 simulated_data <- data.frame(
-  x = c(rnorm(n_points / 2, mean = 0, sd = 0.5), 
-        rnorm(n_points / 2, mean = 0, sd = 0.5)),
-  y = c(rnorm(n_points / 2, mean = 0, sd = 0.5), 
+  x = c(rnorm(n_points, sd = 0.5)),
+  y = c(rnorm(n_points / 2, sd = 0.5), 
         line_length * rnorm(n_points / 2)),
   t = c(rnorm(n_points / 2) * 3, 
-        rnorm(n_points / 2) * 1)
+        rnorm(n_points / 2, sd = 0.5))
 )
 
 wirte_simulated_data(simulated_data, 5)
@@ -129,17 +129,81 @@ wirte_simulated_data(simulated_data, 6)
 
 ### Two Linear Structures
 
+set.seed(123)
+
+line_length <- 3
+
+simulated_data <- data.frame(
+  x = rnorm(n_points * 2, mean = rep(c(-3, 3), each = n_points), sd = 0.5),
+  y = line_length * rnorm(n_points * 2),
+  t = rnorm(n_points * 2, sd = 0.5)
+)
+
 wirte_simulated_data(simulated_data, 7)
 
 ### Two Cylinders
+
+set.seed(123)
+
+z <- seq(0, 2 * pi, length.out = n_points)
+radius <- rep(c(4, 8), each = n_points)
+noise <- rnorm(n_points * 2, sd = 0.5)
+
+simulated_data <- data.frame(
+  x = (radius + noise) * cos(z) ,
+  y = (radius + noise) * sin(z) ,
+  t = rnorm(n_points * 2) * 3
+)
 
 wirte_simulated_data(simulated_data, 8)
 
 ### Two Horizontal Cross Structures
 
+set.seed(123)
+
+cross_size <- 2
+
+simulated_data <- rbind(
+  data.frame(
+    x = c(rnorm(n_points / 2, sd = 0.5), 
+          cross_size * rnorm(n_points / 2)) - 2,
+    y = c(cross_size * rnorm(n_points / 2), 
+          rnorm(n_points / 2, sd = 0.5)) - 2,
+    t = rnorm(n_points) * cross_size - 2
+  ),
+  data.frame(
+    x = c(rnorm(n_points / 2, sd = 0.5), 
+          cross_size * rnorm(n_points / 2)) + 2,
+    y = c(cross_size * rnorm(n_points / 2), 
+          rnorm(n_points / 2, sd = 0.5)) + 2,
+    t = rnorm(n_points) * cross_size + 2
+  )
+)
+
 wirte_simulated_data(simulated_data, 9)
 
 ### Two Vertical Cross Structures
+
+set.seed(123)
+
+line_length <- 3
+
+simulated_data <- rbind(
+  data.frame(
+    x = c(rnorm(n_points, sd = 0.5)) + 2,
+    y = c(rnorm(n_points / 2, sd = 0.5), 
+          line_length * rnorm(n_points / 2)) + 2,
+    t = c(rnorm(n_points / 2) * 3, 
+          rnorm(n_points / 2, sd = 0.5)) + 2
+  ),
+  data.frame(
+    x = c(rnorm(n_points, sd = 0.5)) - 2,
+    y = c(rnorm(n_points / 2, sd = 0.5), 
+          line_length * rnorm(n_points / 2)) - 2,
+    t = c(rnorm(n_points / 2) * 3, 
+          rnorm(n_points / 2, sd = 0.5)) - 2
+  )
+)
 
 wirte_simulated_data(simulated_data, 10)
 
@@ -166,20 +230,27 @@ wirte_simulated_data(simulated_data, 11)
 
 ### Two Overlapping Faulted Linear Structures
 
+set.seed(123)
+
 wirte_simulated_data(simulated_data, 12)
 
 ### Two Overlapping Faulted Cylinders
+
+set.seed(123)
 
 wirte_simulated_data(simulated_data, 13)
 
 ### Two Overlapping Faulted Horizontal Cross Structures
 
+set.seed(123)
+
 wirte_simulated_data(simulated_data, 14)
+
+set.seed(123)
 
 ### Two Overlapping Faulted Vertical Cross Structures
 
 wirte_simulated_data(simulated_data, 15)
-
 
 ## Two Overlapping Smooth Clusters
 
@@ -202,16 +273,24 @@ wirte_simulated_data(simulated_data, 16)
 
 ### Two Overlapping Smooth Linear Structures
 
+set.seed(123)
+
 wirte_simulated_data(simulated_data, 17)
 
 ### Two Overlapping Smooth Cylinders
+
+set.seed(123)
 
 wirte_simulated_data(simulated_data, 18)
 
 ### Two Overlapping Smooth Horizontal Cross Structures
 
+set.seed(123)
+
 wirte_simulated_data(simulated_data, 19)
 
 ### Two Overlapping Smooth Vertical Cross Structures
+
+set.seed(123)
 
 wirte_simulated_data(simulated_data, 20)
