b0180b1d92ad38ca614c58df20e1f851551dfeed
DESKTOP-FI5061K\Owner <angeladk394@gmail.com>
2024-02-28 16:35:10 +0800

    [Add] hst_optics function

--- src/clustering_algorithms/proposed_algorithm.R
+++ src/clustering_algorithms/proposed_algorithm.R
@@ -1,1 +1,112 @@
 # proposed_algorithm.R
+
+# hst_optics
+hst_optics <- function(
+    xyt_df, 
+    xyt_colname=c("x", "y", "t"),
+    eps_s, 
+    eps_t, 
+    min_pts) {
+  
+  # This function orders spatio-temporal points to identify the clustering structure
+  #
+  # Args:
+  #   xyt_df: A data frame containing spatio-temporal points
+  #   xyt_colname: A vector which contains names of columns holding longitude, latitude, and time
+  #   eps_s: Size (radius) of the spatial epsilon neighborhood
+  #   eps_t: Size (radius) of the temporal epsilon neighborhood
+  #   min_pts: Number of minimum points required in the eps_s and eps_t 
+  #            neighborhood for core points (including the point itself).
+  #
+  # Returns:
+  #   A data frame containing order and reachability scores of points
+  
+  # Create a copy of the data frame
+  xyt_df_copy <- xyt_df
+  
+  # Setup unique id for event point
+  xyt_df_copy <- xyt_df_copy %>% mutate(id = 1:nrow(.), .before = xyt_colname[1])
+  
+  # Space for recording order & reachability scores & points to be visited
+  ordered_id <- c()
+  reach_score <- c()
+  points_to_be_visited <- slice(xyt_df_copy, 0)
+  
+  # A function calculate distance from a specific point to other points
+  get_distance_to <- function(one_point, other_points) {
+    sq_dist <- (one_point[1, xyt_colname[1]] - other_points[, xyt_colname[1]])^2 + 
+      (one_point[1, xyt_colname[2]] - other_points[, xyt_colname[2]])^2
+    return(sqrt(sq_dist))
+  }
+  
+  for(iter in 1:nrow(xyt_df_copy)){  
+    
+    # Visit each point one by one
+    message(paste(iter, "/", nrow(xyt_df_copy), sep = ""))
+    
+    if(nrow(points_to_be_visited) == 0){
+      
+      # If there is no point to be visited
+      # Continue with the point at the very beginning of the unvisited points
+      visited_id <- setdiff(xyt_df_copy$id, ordered_id)[1]
+      
+      # Record reachability score
+      reach_score <- reach_score %>% append(Inf)
+      
+    }else{
+      
+      # If there are any points to be visited
+      # Continue with the point with the smallest reachability distance
+      visited_id <- 
+        points_to_be_visited$id[which.min(points_to_be_visited$reach_dist)]
+      
+      # Record reachability score
+      reach_score <- reach_score %>% 
+        append(min(points_to_be_visited$reach_dist))
+      
+    }
+    
+    # Record order
+    ordered_id <- ordered_id %>% append(visited_id)
+    
+    # Find temporal neighbor points
+    t_nbs <- xyt_df_copy %>% 
+      filter(get(xyt_colname[3]) <= (.[visited_id, xyt_colname[3]] + eps_t)) %>% 
+      filter(get(xyt_colname[3]) >= (.[visited_id, xyt_colname[3]] - eps_t))
+    
+    # Find spatio-temporal neighbor points
+    st_nbs <- t_nbs %>% 
+      mutate(dist = get_distance_to(xyt_df_copy[visited_id, ], .)) %>% 
+      filter(dist <= eps_s)
+    
+    if(nrow(st_nbs) >= min_pts){
+      
+      # If visited point is core point
+      # Calculate core distance
+      core_dist <- st_nbs$dist %>% sort() %>% .[min_pts]
+      
+      # Calculate reachability distance
+      st_nbs <- st_nbs %>%
+        rename(reach_dist = dist) %>% 
+        mutate(reach_dist = ifelse(reach_dist < core_dist, 
+                                   core_dist, 
+                                   reach_dist))
+      
+      # Renew points to be visited
+      # Keep rows with unique IDs, 
+      # preferring the one with the smaller reachability distance.
+      points_to_be_visited <- points_to_be_visited %>% 
+        bind_rows(st_nbs) %>%
+        arrange(reach_dist) %>%
+        distinct(id, .keep_all = TRUE)
+      
+    }
+    # Remove visited point from points to be visited
+    points_to_be_visited <- points_to_be_visited %>%
+      filter(!(id %in% ordered_id))
+  }
+  
+  return(data.frame(ordered_id = ordered_id, 
+                    reach_score = reach_score))
+  
+}
